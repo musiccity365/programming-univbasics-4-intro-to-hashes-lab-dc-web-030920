ad62c43b6e56cedf5471f48cb5a6196f1ef0aa71
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-01 20:12:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,11 +25,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-   if read_from_hash[key]
-     puts hash
-   else
-     puts nil
-   end
+   hash[key]
 end
 
 def update_counting_hash(hash, key)
